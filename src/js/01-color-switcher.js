596675d9ce443bead3b632b38b65ccbe30286623
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-09-23 15:12:11 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  timerId: null,
+};
 
+refs.startBtn.addEventListener('click', handleStart);
+refs.stopBtn.addEventListener('click', handleStop);
+
+function handleStart() {
+  refs.startBtn.disabled = true;
+  document.body.style.backgroundColor = getRandomHexColor();
+
+  refs.timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function handleStop() {
+  refs.startBtn.disabled = false;
+  clearInterval(refs.timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
