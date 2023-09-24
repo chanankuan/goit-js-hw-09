775f84c3e7c5242cdda876a65b4199e93d682cdf
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-09-24 12:41:59 +0300

    Add music to the page

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,15 +1,19 @@
 const refs = {
   startBtn: document.querySelector('[data-start]'),
   stopBtn: document.querySelector('[data-stop]'),
+  audio: document.querySelector('audio'),
   timerId: null,
 };
 
+refs.stopBtn.disabled = true;
 refs.startBtn.addEventListener('click', handleStart);
 refs.stopBtn.addEventListener('click', handleStop);
 
 function handleStart() {
   refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
   document.body.style.backgroundColor = getRandomHexColor();
+  refs.audio.play();
 
   refs.timerId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
@@ -18,6 +22,8 @@ function handleStart() {
 
 function handleStop() {
   refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+  refs.audio.pause();
   clearInterval(refs.timerId);
 }
 
